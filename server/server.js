2c6e339613b5fc11f7cc3db6b347c4b1af6af6a7
raju0005 <anderajesh15@gmail.com>
2024-04-22 14:59:00 +0530

    fid

--- server/server.js
+++ server/server.js
@@ -17,8 +17,11 @@ app.post('/', async(req, res) => {
     }
     
 });
+app.get('/hello', async(req, res) => {
+  res.send('hello');
+});
 app.get('/', async(req, res) => {
-  res.json("hello")
+ 
     const uniq_id = req.query.uniq_id;
     try {
       const userData = await UserData.findOne({ uniq_id });
